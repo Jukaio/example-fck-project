233bd3580751d93e93767e88c6b721ea36d61a3e
David <D.Naussed@outlook.de>
2025-02-02 21:15:00 +0100

    Init

---
+++ src/main.cpp
@@ -0,0 +1,40 @@
+
+#include <ecs/fck_ecs.h>
+#include <fck.h>
+#include <game/game_components.h>
+#include <game/game_systems.h>
+
+void app_start_instance(fck_ecs *ecs)
+{
+	fck_ecs_system_add(ecs, game_render_process);
+	fck_ecs_system_add(ecs, game_cammy_setup);
+	// TODO: Remove font loading from fck
+	// Setup and ammend to cammy for now - cammy is just a default sprite with position and size...
+	// Move that all game shit to this layer
+	fck_ecs_system_add(ecs, [](fck_ecs *ecs, fck_system_once_info *info) {
+		fck_ecs_apply_with_entity(ecs, [ecs](fck_ecs::entity_type entity, game_position *position) {
+			game_sprite *sprite = fck_ecs_component_create<game_sprite>(ecs, entity);
+			sprite->texture = gen_assets_png::BubbleGhostCat;
+			sprite->src = {0.0f, 0.0f, 1000.0f, 1000.0f};
+		});
+	});
+}
+
+int main(int argc, char **argv)
+{
+	fck fck;
+	fck_init(&fck, 1);
+
+	fck_instance_info info;
+	info.title = "Example";
+	info.ip = "0.0.0.0";
+	info.source_port = 0;
+	info.destination_port = 0;
+
+	fck_prepare(&fck, &info, app_start_instance);
+
+	int exit_code = fck_run(&fck);
+	SDL_Log("fck - exit code: %d", exit_code);
+
+	return exit_code;
+}
